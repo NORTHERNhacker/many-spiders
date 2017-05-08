22825e11578a2e9d1564a33c8493d434f038b45b
acfun <1732164513@qq.com>
2017-05-09 01:23:35 +0800

    Update freebufspider1.py

--- freebufspider1.py
+++ freebufspider1.py
@@ -1,8 +1,8 @@
 import requests
 from bs4 import BeautifulSoup
-for i in range(1,20):
+for i in range(1,20):#先爬取20个页面，数字当然可以改
     url='http://www.freebuf.com/www.freebuf.com?action=ajax_wenku&year=all&score=all&type=all&tech=0&keyword=&page='+str(i)
     r=requests.get(url)
-    soup= BeautifulSoup(r.text,'lxml')
+    soup= BeautifulSoup(r.text,'lxml')#直接把json数据传递给BeautifulSoup解析
     for i in soup.select('h3 a'):
-        print(i.text.encode('latin-1').decode('unicode_escape'),i.get('href'))
+        print(i.text.encode('latin-1').decode('unicode_escape'),i.get('href'))#解析出的数据是unicode形式，所以要对unicode码进行转码操作
